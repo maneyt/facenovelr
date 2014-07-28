58cec7bdf113e5882f1f894152989b7c96a861ec
Thomas Maney <thomasjmaney@gmail.com>
2014-07-28 13:07:30 -0400

    Debug rebase

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,7 @@
 class DashboardsController < ApplicationController
   def show
-<<<<<<< HEAD
     @thought = Thought.new
     @my_thoughts = current_user.thoughts
-=======
     @photo = Photo.new
->>>>>>> Add photo table, users can upload photos via paperclip
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-<<<<<<< HEAD
   has_many :thoughts, dependent: :destroy
-=======
   has_many :photos
->>>>>>> Add photo table, users can upload photos via paperclip
-=======
   has_many :photos, dependent: :destroy
->>>>>>> Add description column to photos, gitignore public/system
 end
