a36494c8e5a5dc25a11a59ee2edd46c8fe4b8cd8
ChristopherThorsen <Thorsen41@gmail.com>
2014-07-28 11:05:40 -0400

    fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  has_many :thoughts
+  has_many :thoughts, depend: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :thoughts, only: [:create]
+  resources :thoughts, only: [:create, :destroy]
   root to: "dashboards#show"
 end
