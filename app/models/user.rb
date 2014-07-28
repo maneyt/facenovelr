a204dbcc3192428f8d80c1a97077ab4772d75335
Thomas Maney <thomasjmaney@gmail.com>
2014-07-28 15:54:18 -0400

    Check whether request is out when displaying friend request button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,32 @@
 class User < ActiveRecord::Base
   has_many :thoughts, dependent: :destroy
   has_many :photos, dependent: :destroy
-  has_many :friendships
+  has_many :friendships, foreign_key: :friendee_id
+
+  has_many :accepted_friendships, -> { where(accepted: true) }, foreign_key: :friendee_id, class_name: "Friendship"
+  has_many :friends, through: :accepted_friendships, source: :friender
 
   def incoming_friend_requests
-    Friendship.where(friendee_id: id, accepted: false)
+    friendships.where(accepted: false)
   end
 
-  def friends
-    friendships = Friendship.where(friendee_id: id, accepted: true)
-    friendships.map(&:friender)
+  def friends_with?(user)
+    friends.include?(user)
+  end
+
+  def received_friend_request_from?(user)
+    Friendship.exists?(friender_id: user.id, friendee_id: id)
+  end
+
+  def friend_request_from(user)
+    incoming_friend_requests.find_by(friender_id: user.id)
+  end
+
+  def sent_friend_request_to?(user)
+    Friendship.exists?(friender_id: id, friendee_id: user.id)
+  end
+
+  def can_friend(user)
+    self != user && !friends_with?(user) && !sent_friend_request_to?(user)
   end
 end
