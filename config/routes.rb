06a6c06c79fc0d98692391cb3bde8d26dc413187
ChristopherThorsen <Thorsen41@gmail.com>
2014-07-29 10:49:52 -0400

    Show user's photos as thumbnail links to show pg

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create] do 
     resources :thoughts, only: [:create, :destroy]
-    resources :photos, only: [:index]
+    resources :photos, only: [:index, :show]
     resources :friendships, only: [:create, :destroy]
   end
   resources :photos, only: [:create]
