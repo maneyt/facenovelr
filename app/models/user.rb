7531a6fbec1918a28a1bb235cae74b81909b6313
Mike Murphy <mikeymurph77@gmail.com>
2014-07-30 10:02:00 -0400

    New users have default profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  has_attached_file :profile_picture,
+  has_attached_file :profile_picture, 
+    default_url: ActionController::Base.helpers.asset_path("default_profile_image.png"),
     styles: {
-      default_profile_picture: "300x300#",
+      profile_picture: "300x300#",
     }
   validates_attachment :profile_picture, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
