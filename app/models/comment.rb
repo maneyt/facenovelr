b36a25b3132ca5e3d2500cd999354be47696a08c
ChristopherThorsen <Thorsen41@gmail.com>
2014-08-01 13:35:57 -0400

    Users can like photos, thoughts, and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
+  has_many :likes, as: :liked_object, dependent: :destroy
+  has_many :likers, through: :likes, source: :user
+
   validates :body, presence: true
   validates :commentable, presence: true
   validates :user, presence: true
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -3,6 +3,9 @@ class Thought < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :likes, as: :liked_object, dependent: :destroy
+  has_many :likers, through: :likes, source: :user
+
   def self.newest_first
     order("created_at DESC")
   end
